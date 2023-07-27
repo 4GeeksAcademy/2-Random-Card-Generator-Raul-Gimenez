60b9da2d919a57d428f3d9f18f004c95aa6def6f
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-07-27 11:47:21 +0000

    Change style.css

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ let cardDeck = {
   "♥": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
   "♦": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
   "♣": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-  "♠": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  "♠": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 };
 
 function availableColors(cardDeck) {
   let availableColors = [];
   for (const colors in cardDeck) {
-    if (cardDeck[colors].length == 0){
+    if (cardDeck[colors].length == 0) {
       delete cardDeck[colors];
+      if (Object.keys(cardDeck).length == 0) {
+        cardDeck.empty = [" "];
+      }
     }
   }
   for (const keys in cardDeck) {
@@ -24,64 +27,22 @@ function cardColor() {
   return color[randomNumber];
 }
 
+const color = cardColor();
+
 function cardCatch(color) {
   let arrayLength = cardDeck[color].length;
   let randomCardNumber = Math.floor(Math.random() * arrayLength);
-  let card = cardDeck[color].splice(randomCardNumber, 1) + color;
+  let card = cardDeck[color].splice(randomCardNumber, 1);
   return card;
 }
 
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
-console.log(cardCatch(cardColor()));
+const frontNumber = document.querySelector("#frontNumber");
+const upAndBottomCardColor = document.querySelectorAll(
+  "#upCardColor, #bottomCardColor"
+);
 
-console.log(cardDeck);
+upAndBottomCardColor.className = color;
+upAndBottomCardColor.forEach(element => {
+  return element.append(color);
+});
+frontNumber.append(cardCatch(color));
