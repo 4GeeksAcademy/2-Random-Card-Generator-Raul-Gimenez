93d581b9daa9ca74e9872b06de58c76000c7d9df
Raul Gimenez Murga <136567985+Raauul1996@users.noreply.github.com>
2023-07-27 09:58:57 +0000

    Making logical basics in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,87 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let cardDeck = {
+  "♥": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  "♦": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  "♣": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
+  "♠": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
 };
+
+function availableColors(cardDeck) {
+  let availableColors = [];
+  for (const colors in cardDeck) {
+    if (cardDeck[colors].length == 0){
+      delete cardDeck[colors];
+    }
+  }
+  for (const keys in cardDeck) {
+    availableColors.push(keys);
+  }
+  return availableColors;
+}
+
+function cardColor() {
+  let color = availableColors(cardDeck);
+  let randomNumber = Math.floor(Math.random() * color.length);
+  return color[randomNumber];
+}
+
+function cardCatch(color) {
+  let arrayLength = cardDeck[color].length;
+  let randomCardNumber = Math.floor(Math.random() * arrayLength);
+  let card = cardDeck[color].splice(randomCardNumber, 1) + color;
+  return card;
+}
+
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+console.log(cardCatch(cardColor()));
+
+console.log(cardDeck);
